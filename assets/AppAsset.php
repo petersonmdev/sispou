53c6e96cb54e1733994fca8bfa5d15cd304c624c
Jandimar Rocha <mazinhozr@gmail.com>
2019-06-10 12:27:16 -0300

    added appAssets

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -19,7 +19,6 @@ class AppAsset extends AssetBundle
 {
     //public $basePath = '@webroot';
     //public $baseUrl = '@web';
-
     public $sourcePath = '@app/assets/sispou/';
     public $css = [
         'css/layout.css',
@@ -28,15 +27,15 @@ class AppAsset extends AssetBundle
         'libs/fonts/font-sispou/css/flaticon.css',
         'libs/fonts/font-awesome/css/font-awesome.css',
         'https://use.fontawesome.com/releases/v5.6.3/css/all.css',
-        '@vendor\almasaeed2010\adminlte\bower_components\fullcalendar\dist\fullcalendar.min.css',
-        '@vendor\almasaeed2010\adminlte\bower_components\fullcalendar\dist\fullcalendar.print.min.css',
+        // 'vendor/almasaeed2010/adminlte/bower_components/fullcalendar/dist\fullcalendar.min.css',
+        // 'vendor/almasaeed2010/adminlte/bower_components/fullcalendar/dist\fullcalendar.print.min.css',
     ];
     public $js = [
         'js/layout.css',
         'js/inicio.js',
 
-        '@vendor/almasaeed2010/adminlte/bower_components/moment/moment.js',
-        '@vendor/almasaeed2010/adminlte/bower_components/fullcalendar/dist/fullcalendar.min.js',
+        // 'vendor/almasaeed2010/adminlte/bower_components/moment/momen/.js',
+        // 'vendor/almasaeed2010/adminlte/bower_components/fullcalendar/dist/fullcalendar.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
