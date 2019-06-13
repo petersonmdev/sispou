b5a29b81284136da9402625ffd19b2a45ecf268b
Jandimar Rocha <mazinhozr@gmail.com>
2019-06-13 08:37:05 -0300

    added footer and breadcrumb

---
+++ views/layouts/breadcrumb.php
@@ -0,0 +1,14 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+?>
+
+<section class="content-header">
+  <h1>Lembretes</h1>
+  <ol class="breadcrumb">
+    <li><a href="#"> Pousada teste</a></li>
+    <li class="active"> Lembretes</li>
+  </ol>
+</section>
---
+++ views/layouts/footer.php
@@ -0,0 +1,13 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+?>
+
+<footer class="main-footer footer-sispou">
+  <div class="pull-right hidden-xs">
+    <b>Version</b> 1.0
+  </div>
+  <strong>&copy; 2019 Sispou -</strong> Todos direitos reservados
+</footer>
